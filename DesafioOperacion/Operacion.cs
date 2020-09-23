26ecc3d946d07a8fa64599cecec04182736426f4
Franco Veloci <fveloci@hotmail.com>
2020-09-23 20:54:55 -0300

    Primer commit

---
+++ DesafioOperacion/Operacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioOperacion
+{
+    class Operacion
+    {
+        public static void sumar(int num1, int num2)
+        {
+            Console.WriteLine("{0} + {1} = {2}",num1, num2,num1 + num2);
+        }
+        public static void restar(int num1, int num2)
+        {
+            Console.WriteLine("{0} - {1} = {2}",num1,num2,num1 - num2);
+        }
+    }
+}
---
+++ DesafioOperacion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioOperacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Ingrese un primer numero:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingrese un segundo numero:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Operacion.sumar(num1,num2);
+                Operacion.restar(num1,num2);
+
+
+                Console.WriteLine("Toque cualquier tecla para finalizar...");
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);                
+            }
+           
+
+        }
+    }
+}
